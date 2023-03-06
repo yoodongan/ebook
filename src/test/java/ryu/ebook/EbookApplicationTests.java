14005887fbd816d1dc06f61b86bd6cd255e19155
yoodongan <dbehddks519@naver.com>
2023-03-06 21:05:24 +0900

    프로젝트 세팅

---
+++ src/test/java/ryu/ebook/EbookApplicationTests.java
@@ -0,0 +1,13 @@
+package ryu.ebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
