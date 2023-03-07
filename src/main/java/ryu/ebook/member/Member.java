4078e74b48422d98a4abda8a959fb6d63d36b4b8
yoodongan <dbehddks519@naver.com>
2023-03-07 15:42:13 +0900

    인가 작업 수행을 위한 GrantedAuthority 추가 - ROLE_MEMBER, ROLE_AUTHOR 추가

--- src/main/java/ryu/ebook/member/Member.java
+++ src/main/java/ryu/ebook/member/Member.java
@@ -3,7 +3,13 @@ package ryu.ebook.member;
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.util.StringUtils;
 import ryu.ebook.base.BaseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
@@ -14,5 +20,15 @@ public class Member extends BaseEntity {
 
     private String nickname;
     private String email;
+
+    public List<GrantedAuthority> genAuthorities() {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_MEMBER"));
+
+        if(StringUtils.hasText(nickname)) {   // 작가 필명(nickname) 이 있는 경우에만 권한 부여.
+            grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_AUTHOR"));
+        }
+        return grantedAuthorities;
+    }
 
 }
