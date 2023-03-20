1564c8b68a570a8007531420e2948532b9421689
yoodongan <dbehddks519@naver.com>
2023-03-20 11:36:09 +0900

    BaseEntity 추가 - 어노테이션 추가, 필드 수정

--- src/main/java/ryu/ebook/base/BaseEntity.java
+++ src/main/java/ryu/ebook/base/BaseEntity.java
@@ -1,11 +1,36 @@
 package ryu.ebook.base;
 
-import jakarta.persistence.*;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+
+@Getter
 @MappedSuperclass
+@SuperBuilder
 @EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class BaseEntity {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue
     private Long id;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+    public BaseEntity(Long id) {
+        this.id = id;
+    }
 }
