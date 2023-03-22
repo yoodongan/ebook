da06d000aa3a096512872c12ef25509ea43c8626
yoodongan <dbehddks519@naver.com>
2023-03-22 14:30:36 +0900

    CustomerUserDetailsService 추가 - User 를 상속받는 MemberContext 반환.(인증 정보 담겨 있음) - UserDetailsService 를 구현해 인증 정보 객체를 반환하는 클래스

--- src/main/java/ryu/ebook/security/service/CustomerUserDetailsService.java
+++ src/main/java/ryu/ebook/security/service/CustomerUserDetailsService.java
@@ -1,25 +1,21 @@
-package ryu.ebook.security;
+package ryu.ebook.security.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import ryu.ebook.member.Member;
 import ryu.ebook.member.repository.MemberRepository;
-
-import java.util.Optional;
+import ryu.ebook.security.dto.MemberContext;
 
 @Service
 @RequiredArgsConstructor
-public class CustomUserDetailsService implements UserDetailsService {
+public class CustomerUserDetailsService implements UserDetailsService {
     private final MemberRepository memberRepository;
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Member member = memberRepository.findByUsername(username).get();
-
-        return new User(member.getUsername(), member.getPassword(), member.genAuthorities());
+        return new MemberContext(member, member.genAuthorities());
     }
 }
