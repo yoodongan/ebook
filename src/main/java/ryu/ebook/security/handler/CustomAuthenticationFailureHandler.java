110da4d6c7b29aef71729543faa32f17a8a9ed5c
yoodongan <dbehddks519@naver.com>
2023-03-22 14:34:05 +0900

    CustomAuthFailureHandler 추가 - 인증 실패 시 다시 로그인 화면으로 리다이렉트

---
+++ src/main/java/ryu/ebook/security/handler/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,25 @@
+package ryu.ebook.security.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+    private final RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        String url = "/member/login";
+        redirectStrategy.sendRedirect(request, response, url);
+    }
+}
